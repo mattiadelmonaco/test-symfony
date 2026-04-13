a28c7c90facf12b39ab0ee89d951c26446861054
mattiadelmonaco <mattia.delmonaco@gmail.com>
2026-04-13 19:48:53 +0200

    feat: add method new

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\PostRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\DateImmutableType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,5 +30,25 @@ final class BlogController extends AbstractController
         $post = $postRepository->findOneBy(['slug' => $slug]);
 
         return $this->render('blog/show.html.twig', ['post' => $post]);
+    }
+
+    #[Route('/post/new', name: 'app_post_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug(strtolower(str_replace(' ', '-', $post->getTitle())));
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setIsPublished(false);
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('blog/new.html.twig', ['form' => $form]);
     }
 }
