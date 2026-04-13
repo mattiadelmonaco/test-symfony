341883db8d25e313b1c848bd1159a798d7f5b863
mattiadelmonaco <mattia.delmonaco@gmail.com>
2026-04-13 19:08:48 +0200

    feat: edit blogcontroller, index and show views

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,25 +10,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class BlogController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
 
-        $posts = [
-            ['title' => 'Primo post', 'slug' => 'primo-post'],
-            ['title' => 'Secondo post', 'slug' => 'secondo-post'],
-        ];
+        $posts = $postRepository->findAll();
 
         return $this->render('blog/index.html.twig', ['posts' => $posts,]);
     }
 
     #[Route('post/{slug}', name: 'app_post_show')]
-    public function show(string $slug): Response
+    public function show(string $slug, PostRepository $postRepository): Response
     {
-        $post = [
-            'title' => 'Titolo del post',
-            'slug' => $slug,
-            'content' => 'Contenuto del post',
-        ];
+        $post = $postRepository->findOneBy(['slug' => $slug]);
 
         return $this->render('blog/show.html.twig', ['post' => $post]);
     }
