bbaab6578800e381d516fcbb0d95aba4981664a4
mattiadelmonaco <mattia.delmonaco@gmail.com>
2026-04-13 18:58:11 +0200

    feat: add faker bundle and edit and execute appfixtures with fake datas

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('it_IT');
+
+        // Creo 3 utenti
+        $users = [];
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+            $user->setUsername($faker->userName());
+            $user->setEmail($faker->email());
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Creo 10 post
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence(4));
+            $post->setSlug($faker->slug());
+            $post->setContent($faker->paragraphs(3, true));
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setIsPublished($faker->boolean(80));
+            $post->setAuthor($users[array_rand($users)]);
+            $manager->persist($post);
+        }
 
         $manager->flush();
     }
